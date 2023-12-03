bee9ba36913e6e7dba640086a895f585443e96da
jacksonneal <jacksontneal@gmail.com>
2023-12-02 20:16:46 -0500

    day 2

--- aoc/__main__.py
+++ aoc/__main__.py
@@ -68,6 +68,70 @@ def day1b() -> int:
     return total
 
 
+def day2a() -> int:
+    n_red_cubes = 12
+    n_green_cubes = 13
+    n_blue_cubes = 14
+
+    lines = get_lines("day2.txt")
+
+    total = 0
+    for line in lines:
+        sections = line.strip().split(";")
+        sections = sections[0].strip().split(":") + sections[1:]
+
+        game_ok = True
+        for section in sections[1:]:
+            if not game_ok:
+                break
+            for cube_count in section.strip().split(","):
+                count_and_color = cube_count.strip().split(" ")
+                count = int(count_and_color[0])
+                color = count_and_color[1]
+                if (
+                    color == "red"
+                    and count > n_red_cubes
+                    or color == "green"
+                    and count > n_green_cubes
+                    or color == "blue"
+                    and count > n_blue_cubes
+                ):
+                    game_ok = False
+                    break
+
+        game_num = int(sections[0].split(" ")[1])
+        if game_ok:
+            total += game_num
+    return total
+
+
+def day2b() -> int:
+    total = 0
+    lines = get_lines("day2.txt")
+    for line in lines:
+        min_red = 0
+        min_blue = 0
+        min_green = 0
+        sections = line.strip().split(";")
+        sections = [*sections[0].split(":") + sections[1:]]
+        for section in sections[1:]:
+            for cube_count in section.split(","):
+                count_and_color = cube_count.strip().split(" ")
+                count = int(count_and_color[0])
+                color = count_and_color[1]
+                if color == "red":
+                    min_red = max(min_red, count)
+                elif color == "green":
+                    min_green = max(min_green, count)
+                elif color == "blue":
+                    min_blue = max(min_blue, count)
+        total += min_red * min_green * min_blue
+    return total
+
+
 if __name__ == "__main__":
-    assert day1a() == 54159
-    assert day1b() == 53866
+    # assert day1a() == 54159
+    # assert day1b() == 53866
+
+    # assert day2a() == 2679
+    assert day2b() == 77607
