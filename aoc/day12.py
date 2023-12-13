55cf749e549346c3cb1188faeb8caab1d68768f0
jacksonneal <jacksontneal@gmail.com>
2023-12-13 10:25:50 -0500

    day12

--- aoc/day12.py
+++ aoc/day12.py
@@ -1,4 +1,5 @@
 # ruff: noqa: T201
+from functools import cache
 
 from aoc.util import get_lines
 
@@ -21,7 +22,10 @@ def take_hit(groups: list[int]) -> list[int] | None:
     return taken
 
 
+@cache
 def num_combos(springs: str, groups: list[int], *, mid_group: bool = False) -> int:
+    groups = list(groups)
+
     if len(springs) == 0:
         return 1 if len(groups) == 0 or len(groups) == 1 and groups[0] == 0 else 0
 
@@ -32,22 +36,22 @@ def num_combos(springs: str, groups: list[int], *, mid_group: bool = False) -> i
         next_groups = take_miss(groups, mid_group=mid_group)
         if next_groups is None:
             return 0
-        return num_combos(rest_springs, next_groups, mid_group=False)
+        return num_combos(rest_springs, tuple(next_groups), mid_group=False)
     if cur_spring == "#":
         next_groups = take_hit(groups)
         if next_groups is None:
             return 0
-        return num_combos(rest_springs, next_groups, mid_group=True)
+        return num_combos(rest_springs, tuple(next_groups), mid_group=True)
     if cur_spring == "?":
         hit = 0
         next_groups = take_hit(groups)
         if next_groups is not None:
-            hit = num_combos(rest_springs, next_groups, mid_group=True)
+            hit = num_combos(rest_springs, tuple(next_groups), mid_group=True)
 
         miss = 0
         next_groups = take_miss(groups, mid_group=mid_group)
         if next_groups is not None:
-            miss = num_combos(rest_springs, next_groups, mid_group=False)
+            miss = num_combos(rest_springs, tuple(next_groups), mid_group=False)
 
         return hit + miss
 
@@ -55,14 +59,28 @@ def num_combos(springs: str, groups: list[int], *, mid_group: bool = False) -> i
 
 
 def part_a() -> int:
-    lines = get_lines("day12-sample.txt")
+    lines = get_lines("day12.txt")
     total = 0
     for line in lines:
         springs, groups = line.split()
         groups = [int(x) for x in groups.split(",")]
-        total += num_combos(springs, groups)
+        total += num_combos(springs, tuple(groups))
+    return total
+
+
+def part_b() -> int:
+    lines = get_lines("day12.txt")
+    total = 0
+    for line in lines:
+        springs, groups = line.split()
+        springs = "?".join([springs] * 5)
+        groups = ",".join([groups] * 5)
+        print(springs, groups)
+        groups = [int(x) for x in groups.split(",")]
+        total += num_combos(springs, tuple(groups))
     return total
 
 
 if __name__ == "__main__":
-    print(part_a())
+    # assert part_a() == 7732
+    assert part_b() == 4500070301581
