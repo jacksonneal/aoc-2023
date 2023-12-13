80edf5a307072f39dc3ec2fd8637c58b0539f283
jacksonneal <jacksontneal@gmail.com>
2023-12-13 09:58:19 -0500

    work on day12

--- aoc/day12.py
+++ aoc/day12.py
@@ -3,51 +3,55 @@
 from aoc.util import get_lines
 
 
+def take_miss(groups: list[int], *, mid_group: bool) -> list[int] | None:
+    if len(groups) == 0:
+        return []
+    if groups[0] == 0:
+        return groups[1:]
+    if mid_group:
+        return None
+    return groups.copy()
+
+
+def take_hit(groups: list[int]) -> list[int] | None:
+    if len(groups) == 0 or groups[0] == 0:
+        return None
+    taken = groups.copy()
+    taken[0] -= 1
+    return taken
+
+
 def num_combos(springs: str, groups: list[int], *, mid_group: bool = False) -> int:
+    if len(springs) == 0:
+        return 1 if len(groups) == 0 or len(groups) == 1 and groups[0] == 0 else 0
+
     cur_spring = springs[0]
     rest_springs = springs[1:]
-    n_springs = len(springs)
-
-    cur_group = groups[0]
-    n_groups = len(groups)
 
     if cur_spring == ".":
-        if n_springs == 1 and n_groups == 1 and cur_group == 0:
-            return 1
-        if not mid_group:
-            return num_combos(rest_springs, groups.copy(), mid_group=False)
-        return 0
-
+        next_groups = take_miss(groups, mid_group=mid_group)
+        if next_groups is None:
+            return 0
+        return num_combos(rest_springs, next_groups, mid_group=False)
     if cur_spring == "#":
-        if n_springs == 1 and n_groups == 1 and cur_group == 1:
-            return 1
-        groups[0] -= 1
-        if cur_group == 1:
-            return num_combos(rest_springs, groups[1:].copy(), mid_group=False)
-        return num_combos(rest_springs, groups.copy(), mid_group=True)
+        next_groups = take_hit(groups)
+        if next_groups is None:
+            return 0
+        return num_combos(rest_springs, next_groups, mid_group=True)
+    if cur_spring == "?":
+        hit = 0
+        next_groups = take_hit(groups)
+        if next_groups is not None:
+            hit = num_combos(rest_springs, next_groups, mid_group=True)
 
-    take_groups = groups.copy()
-    take_groups[0] -= 1
-    take = num_combos(rest_springs, take_groups, mid_group=True)
+        miss = 0
+        next_groups = take_miss(groups, mid_group=mid_group)
+        if next_groups is not None:
+            miss = num_combos(rest_springs, next_groups, mid_group=False)
 
-    if (
-        n_springs == 1
-        and cur == "."
-        and not mid_group
-        and n_groups == 1
-        and groups[0] == 0
-    ):
-        return 1
+        return hit + miss
 
-    if len(springs) == 1 and springs[0] == "#" and len(groups) == 1 and groups[0] == 1:
-        return 1
-
-    if len(springs) == 1 and springs[0] == "?":
-        x = 0
-        if
-        return 1
-
-    return 0
+    return 1_000_000
 
 
 def part_a() -> int:
@@ -57,7 +61,6 @@ def part_a() -> int:
         springs, groups = line.split()
         groups = [int(x) for x in groups.split(",")]
         total += num_combos(springs, groups)
-        print(total)
     return total
 
 
